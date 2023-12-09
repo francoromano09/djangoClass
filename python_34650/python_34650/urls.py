07c86f1903b8d8841199d0636397987b7713ce56
franco romano <francoromano@alu.frt.utn.edu.ar>
2023-12-09 18:59:56 -0300

    userss

--- python_34650/python_34650/urls.py
+++ python_34650/python_34650/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from python_34650.views import hola_mundo,otra_mas,fecha_actual,vista_con_edad,\
     vista_con_template, saludo_desde_template
 
-from products.views import create_product,list_products,list_categories,create_category
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,8 +29,5 @@ urlpatterns = [
     path('vista-con-template/', vista_con_template),
     path('saludo-desde-template/', saludo_desde_template),
 
-    path('create-product/',create_product),
-    path('list-products/',list_products),
-    path('create-category/<str:name>/',create_category),
-    path('list-categories/',list_categories),
+    path('products/', include('products.urls')),
 ]
