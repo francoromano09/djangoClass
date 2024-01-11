044a6358ca8db2936d1d2cb15b4a35c3b1519170
franco romano <francoromano@alu.frt.utn.edu.ar>
2024-01-11 08:06:14 -0300

    actualizar

--- python_34650/providers/urls.py
+++ python_34650/providers/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from providers.views import providers_list,providers_create,providers_update
 
 
 urlpatterns = [
-    
+    path('providers-list/',providers_list,name='providers_list'),
+    path('create-provider/',providers_create,name='providers_create'),
+    path('update-provider/<int:id>/',providers_update,name='providers_update'),
 ]
--- python_34650/providers/views.py
+++ python_34650/providers/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
+from providers.models import Provider
+from providers.forms import ProviderForm
 
+def providers_list(request):
+    providers = Provider.objects.filter(is_active=True) #Alineacion logica
+    context = {
+        'providers':providers
+    }
+    return render(request, 'providers/providers_list.html',context=context)
 # Create your views here.
+def providers_create(request):
+    if request.method == 'GET':
+        context = {
+            'form' : ProviderForm()
+    }
+        return render(request, 'providers/create_provider.html',context=context)
+
+    elif request.method == 'POST':
+        form = ProviderForm(request.POST)
+    if form.is_valid():
+       #Creamos el producto
+        Provider.objects.create(
+            name = form.cleaned_data['name'],
+            address = form.cleaned_data['address'],
+            phone_number = form.cleaned_data['phone_number'],
+            email = form.cleaned_data['email'],
+            condition = form.cleaned_data['condition'],
+        )
+        context = {
+            'message':'Proveedor creado exitosamente'
+        }
+    else:
+        context = {
+            'form_errors': form.errors,
+            'form':ProviderForm()
+        }
+    return render(request, 'providers/create_provider.html',context=context)
+        
+        #Mostramos el formulario con los errores
+
+def providers_update(request,id):
+    provider = Provider.objects.get(id=id)
+
+    if request.method == 'GET':
+        context = {
+            'form' : ProviderForm(
+                initial = {
+                    'name':provider.name,
+                    'address':provider.address,
+                    'phone_number':provider.phone_number,
+                    'email':provider.email,
+                    'condition':provider.condition,
+                }
+            )
+    }
+        return render(request, 'providers/update_provider.html',context=context)
+
+    elif request.method == 'POST':
+        form = ProviderForm(request.POST)
+        if form.is_valid():
+            #Actualizamos el producto
+            provider.name = form.cleaned_data['name'],
+            provider.address = form.cleaned_data['address'],
+            provider.phone_number = form.cleaned_data['phone_number'],
+            provider.email = form.cleaned_data['email'],
+            provider.condition = form.cleaned_data['condition'],
+            provider.save()
+        context = {
+            'message':'Proveedor Actualizado exitosamente'
+        }
+    else:
+        context = {
+            'form_errors': form.errors,
+            'form':ProviderForm()
+        }
+    return render(request, 'providers/update_provider.html',context=context)
+        
+        #Mostramos el formulario con los errores
